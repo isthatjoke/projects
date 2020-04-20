d47772df96763f33204f3be6f030aa6ec4febc68
Timur Zverev <zverevta@gmail.com>
2020-04-20 20:48:33 +0300

    right choice

--- database/lesson_9/lesson-9.sql
+++ database/lesson_9/lesson-9.sql
@@ -1,120 +1,153 @@
-/*
-Пусть задан некоторый пользователь. 
-Из всех друзей этого пользователя найдите человека, 
-который больше всех общался с нашим пользователем.
-*/
-select * from friendship_statuses; 
+/*В базе данных shop и sample присутствуют одни и те же таблицы, учебной базы данных.
+Переместите запись id = 1 из таблицы shop.users в таблицу sample.users. 
+Используйте транзакции.*/
+
+use sample;
+select * from users;
 desc users;
-desc friendship_statuses;
-select first_name from users where id in (28, 58, 151, 26, 86, 120);
-select * from friendship where friend_id = 93;
-select * from friendship where user_id = 93;
 
-select tmp.first_name, count(*) summ from (select u.id, first_name from users u 
-  join messages m on u.id = from_user_id
-  join friendship f ON u.id = f.user_id where friend_id = 93 
-  and status_id = (select fs.id from friendship_statuses fs where name = "confirmed") 
-  and m.to_user_id = 93)tmp
-   group by first_name
-   order by summ desc limit 1;
-  
-  
-select tmp.first_name, count(*) summ from (select u.id, first_name from users u 
-  join messages m on u.id = from_user_id
-  join friendship f ON u.id = f.user_id where f.friend_id in (select user_id from friendship where user_id = 93 or friend_id = 93)
-  and status_id = (select fs.id from friendship_statuses fs where name = "confirmed") 
-  and m.to_user_id = 93)tmp
-   group by first_name
-   order by summ desc limit 1;
+use shop;
 
-select first_name, f.friend_id, fs.id, count(*)
-  from users u 
-  join friendship f on f.friend_id = u.id
-  join friendship_statuses fs on fs.id = 3
-  join messages m on from_user_id = f.friend_id 
-  where f.user_id = 93 and m.to_user_id = 93
-  group by first_name, f.friend_id, fs.id;
+create table users_2 (
+  id serial,
+  name varchar(255) default null,
+  birthday_at date default null,
+  created_at datetime default current_timestamp,
+  updated_at datetime default current_timestamp on update current_timestamp
+);
+desc users_2;
+
+start transaction;
+insert into sample.users_2 select * from shop.users where id = 1;
+delete from shop.users where id = 1;
+commit;
+select * from users_2;
+select * from shop.users;
+
+/*Создайте представление, которое выводит название name товарной позиции 
+из таблицы products и соответствующее название каталога name из таблицы catalogs.*/
+select * from products;
+select * from catalogs;
+create view prod as select p.name product, c.name catalog_name from shop.products p 
+join shop.catalogs c on p.catalog_id = c.id;
+show tables;
+select * from prod;
+drop view prod;
+
+select time_format(now(),"%H:%i:%s");
+
+
+
+/*Создайте двух пользователей которые имеют доступ к базе данных shop. 
+Первому пользователю shop_read должны быть доступны только запросы на чтение данных, 
+второму пользователю shop — любые операции в пределах базы данных shop*/
+
+create user user_1 identified with sha256_password by '1234';
+create user user_2 identified with sha256_password by '5678';
+grant select on shop.* to user_1;
+grant all on shop.* to user_2;
+
+show grants;
+
+/*Создайте хранимую функцию hello(), которая будет возвращать приветствие, 
+в зависимости от текущего времени суток. С 6:00 до 12:00 функция должна 
+возвращать фразу "Доброе утро", с 12:00 до 18:00 функция должна возвращать 
+фразу "Добрый день", с 18:00 до 00:00 — "Добрый вечер", с 00:00 до 6:00 — "Доброй ночи".*/
+
+drop function if exists hello;
+create function hello()
+returns varchar(30) deterministic
+begin 
+	declare some_text varchar(30);
+    if(select curtime() between ("06:00:01") and ("12:00:00")) then
+    set some_text = "Доброе утро";
+    elseif(select curtime() between ("12:00:01") and ("18:00:00")) then
+    set some_text = "Добрый день";
+    elseif(select curtime() between ("18:00:01") and ("23:59:59")) then
+    set some_text = "Добрый вечер";
+    else
+    set some_text = "Доброй ночи"; 
+	end if;
+return some_text;
+end;
+
+select hello();
+
+
+
+/*В таблице products есть два текстовых поля: name с названием товара 
+и description с его описанием. Допустимо присутствие обоих полей или одно из них. 
+Ситуация, когда оба поля принимают неопределенное значение NULL неприемлема. 
+Используя триггеры, добейтесь того, чтобы одно из этих полей или оба поля были заполнены. 
+При попытке присвоить полям NULL-значение необходимо отменить операцию.*/
+
+use shop;
+desc products;
+
+drop trigger if exists check_prod_null_update;
+create trigger check_prod_null_update before update on products
+for each row
+begin
+	if new.name is NULL and new.description is NULL then
+	signal sqlstate '45000' set message_text = "using NULL in description and name is forbidden while making update";
+end if;
+end;
+
+drop trigger if exists check_prod_null_insert;
+
+create trigger check_prod_null_insert before insert on products
+for each row
+begin 
+	if new.name is NULL and new.description is NULL  then
+	signal sqlstate '45001' set message_text = "using NULL in description and name is forbidden while making insert";
+end if;
+end;
+
+select * from products;
+update products set name = NULL where id = 1;
+update products set description = NULL where id = 1;
+
+insert into products (name, description, price, catalog_id) values (
+  NULL, NULL, NULL, '1');
+
+
+
+/*(по желанию) Напишите хранимую функцию для вычисления произвольного числа Фибоначчи. 
+Числами Фибоначчи называется последовательность в которой число равно сумме двух предыдущих чисел. 
+Вызов функции FIBONACCI(10) должен возвращать число 55.*/
  
-/*select distinct first_name, m.body 
-  from users u 
-  join messages m on m.from_user_id = u.id 
-  join friendship f on f.status_id = 3 and f.friend_id = u.id or f.user_id = u.id;
-*/ 
-select tmp.name id, count(tmp.body) from (select distinct m.from_user_id name, m.body 
-  from friendship f 
-  right join messages m on m.to_user_id = 93
-  where f.status_id = 3 and f.friend_id = 93 or f.user_id = 93)tmp
-  group by id;
  
- select * from messages where to_user_id = 93;
- 
-/*
-Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей.
-*/
-select sum(summ_likes) likes_summary from (select tmp.summ summ_likes from 
-(select u.id, u.first_name, count(*) summ, p.birthdate day_of_birth from users u
-left join
-profiles p on u.id = p.user_id 
-left join 
-likes l on u.id = target_id where target_type_id = (select id from target_types where name = "users")
-group by u.id, u.first_name, p.birthdate)tmp
-order by tmp.day_of_birth desc limit 10) summ_likes;
+drop function if exists fibonacci;
+create function fibonacci(value int)
+returns int deterministic
+begin 
+	declare summ, first_val, second_val, i int;
+	set first_val = 1;
+	set second_val = 1;
+	set i = 0;
+	set summ = 0;
+    if (value = 0) then
+    set summ = 0;
+    elseif (value = 1 or value = 2) then 
+    set summ = 1;
+    elseif (value < 0) then
+    set summ = 0;
+   else
+    set value = value - 2;
+	while i < value do
+	set summ = first_val + second_val;
+	set first_val = second_val;
+	set second_val = summ;
+	set i = i + 1;
+	end while;
+	end if;
+return summ;
+end;
 
-/*
-Определить кто больше поставил лайков (всего) - мужчины или женщины?
-*/ 
-
-select gender, count(*) summ from 
-  profiles p 
-  join 
-  likes l on p.user_id = l.user_id
-  group by gender
-  order by summ desc limit 1;
-
-/*
-Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети
-*/
-
-
-select tmp.id, sum(summ_mess) summary from
-  ((select u.id, count(*) summ_mess
-    from users u
-  join
-    messages m on u.id = m.from_user_id 
-   group by u.id)
-  union
-   (select u.id, count(*) summ_likes
-    from users u
-  join 
-    likes l on u.id = l.user_id
-   group by u.id)) tmp
-  group by tmp.id
-  order by summary asc limit 10;
- 
--- the correct answer is below
-
-select tmp.id, (tmp.summ_mess + tmp.summ_likes) summ from 
-(select u.id id, count(distinct(m.id)) summ_mess, count(distinct(l.id)) summ_likes from users u
-left join messages m on m.from_user_id = u.id 
-join likes l on l.user_id = u.id
-group by u.id)tmp
-order by summ asc limit 10;
-
-select u.id id, (count(distinct(m.id)) + count(distinct(l.id))) summ_likes from users u
-left join messages m on m.from_user_id = u.id 
-join likes l on l.user_id = u.id
-group by u.id
-order by summ_likes asc limit 10;
-
-select u.id id, (count(distinct(m.id)) + count(distinct(l.id))) summ_likes from users u
-left join messages m on m.from_user_id = u.id 
-left join likes l on l.user_id = u.id
-group by u.id
-order by summ_likes asc limit 10;
+select fibonacci(10); 
 
 
-select * from messages m where m.from_user_id = 172;
-select * from likes l where l.user_id = 172;
 
-select * from messages limit 10;
+
+
 
